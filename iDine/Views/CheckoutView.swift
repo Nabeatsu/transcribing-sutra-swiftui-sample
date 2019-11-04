44d77516c5c85497cd3799119263559bbcd52124
t__nabe <tanabe.1478@gmail.com>
2019-11-04 22:37:31 +0900

    Formに使えるControlをいくつか使用してフォーム画面の実装

---
+++ iDine/Views/CheckoutView.swift
@@ -0,0 +1,56 @@
+//
+//  CheckoutView.swift
+//  iDine
+//
+//  Created by 田辺信之 on 2019/11/04.
+//  Copyright © 2019 田辺信之. All rights reserved.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    @EnvironmentObject var order: Order
+    @State private var paymentType = 0
+    @State private var addLoyaltyDetails = false
+    @State private var loyaltyNumber = ""
+    @State private var tipAmount = 1
+    
+    static let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
+    static let tipAmounts = [10, 15, 20, 25, 0]
+    
+    var body: some View {
+        Form {
+            Section {
+                Picker("How do you want to pay?", selection: $paymentType) {
+                    ForEach(0..<Self.paymentTypes.count) {
+                        Text(Self.paymentTypes[$0])
+                    }
+                }
+                Toggle(isOn: $addLoyaltyDetails.animation()) {
+                    Text("Add iDine loyalty card")
+                }
+                TextField("Enter your iDine ID", text: $loyaltyNumber)
+            }
+            Section(header: Text("Add a tip?")) {
+                Picker("Percentage:", selection: $tipAmount) {
+                    ForEach(0..<Self.tipAmounts.count) {
+                        Text("\(Self.tipAmounts[$0])%")
+                    }
+                }.pickerStyle(SegmentedPickerStyle())
+            }
+            Section(header: Text("Total: $100")) {
+                Button("Confirm order") {
+                    // plac the order
+                }
+            }
+        }.navigationBarTitle(Text("Payment"), displayMode: .inline)
+    }
+
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static let order = Order()
+    static var previews: some View {
+        CheckoutView().environmentObject(order)
+    }
+}
